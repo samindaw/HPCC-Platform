62673d28d6b694f8553b3f4f1d4c51ce93d94e4c
samindaw <samindaw@gmail.com>
2018-06-26 22:37:49 -0400

    updated mpi_wrapper for send/recv function supporting timeout instead of non-blocking function calls

--- system/mp/mplog.hpp
+++ system/mp/mplog.hpp
@@ -73,15 +73,12 @@ extern mp_decl bool disconnectLogMsgListenerFromChildOwn(INode * childNode);
     #include <stdarg.h>
     #include <utility>
     #include <iostream>
-    #include <cxxabi.h>
-    #include <execinfo.h>
-    #define _nl std::endl
+    #define nl std::endl
     extern thread_local int debug_counter;
     extern thread_local int debug_thread_id;
     extern int global_proc_rank;
-//    int _getFuncName(char* &funcName, int &funcLength, int skipDepth);
     void trace_print_func_data(std::stringstream &stream, int first_arg);
-    std::string trace_prefix(int skipDepth = 0);
+    std::string trace_prefix();
     template<typename First, typename ...Rest>
     void trace_print_func_data(std::stringstream &stream, int first_arg, First && first, Rest && ...rest){
         stream << ((first_arg)?"":",") << first;
@@ -90,22 +87,15 @@ extern mp_decl bool disconnectLogMsgListenerFromChildOwn(INode * childNode);
     template<typename ...Rest>
     void trace_print_func(const char* funcName, Rest && ...rest){
         std::stringstream stream;
-        stream << trace_prefix(1) << "["<<funcName << "]"<<": (";
+        stream << trace_prefix() << funcName << "(";
         trace_print_func_data(stream, 1, std::forward<Rest>(rest)...);
         std::cout<<stream.str();
     }
-//    template<typename ...Rest>
-//    void trace_print_func(Rest && ...rest){
-//        std::stringstream stream;
-//        stream << trace_prefix() << "(";
-//        trace_print_func_data(stream, 1, std::forward<Rest>(rest)...);
-//        std::cout<<stream.str();
-//    }
 
-    #define _T(x) { std::stringstream stream; stream << trace_prefix() << x << _nl; std::cout << stream.str(); }
+    #define _T(x) { std::stringstream stream; stream << trace_prefix() << x << nl; std::cout << stream.str(); }
     #define _TF(...) trace_print_func(__VA_ARGS__)
 #else
-    #define _nl
+    #define nl
     #define _T(x)
     #define _TF(...)
 #endif
