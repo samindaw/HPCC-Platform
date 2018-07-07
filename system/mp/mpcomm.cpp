618a7c34f516a12a340c8355bb28afd91d879754
samindaw <samindaw@gmail.com>
2018-07-07 01:45:02 -0400

    converting all MPI commands to C++ versions + introducing mpi framework start/stop functions + adding single send test case

--- system/mp/mpcomm.cpp
+++ system/mp/mpcomm.cpp
@@ -3121,7 +3121,7 @@ mptag_t createReplyTag()
 
 ICommunicator *createCommunicator(IGroup *group, bool outer)
 {
-    if (std::getenv(MPI_ENV))
+    if (getenv(MPI_ENV) && !outer)
            return createMPICommunicator(group);
     else
     {
